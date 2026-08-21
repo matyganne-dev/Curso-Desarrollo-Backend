d31287b3a020ef4174a7803699939694948a19b9
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-08-21 07:56:05 -0300

    Agregar archivos .gitignore e index.js con lógica para mostrar ficha de videojuego

---
+++ semana-01/tp-01-fundamentos-node/index.js
@@ -0,0 +1,61 @@
+
+
+const fs = require("node:fs");
+const path = require("node:path");
+
+/*
+Recibir el nombre del estudiante mediante process.argv.
+-Utilizar un nombre alternativo cuando no se proporcione un argumento.
+-Mostrar process.version y process.platform.
+*/
+
+
+const nombre = process.argv[2] ?? "Estudiante";
+console.log(`Bienvenido, ${nombre}`);
+
+/*
+Crear un objeto con estas propiedades
+titulo          Texto       Título del videojuego
+estudio         Texto       Estudio o empresa responsable
+anio Número     Año de      publicación
+plataformas     Arreglo     Al menos dos elementos
+multijugador    Booleano    true o false
+*/
+
+const videojuego = {
+    titulo: "Cyberpunk 2077",
+    estudio: "CD PROJEKT RED",
+    anio: 2020,
+    plataformas: [
+        "PC",
+        "PlayStation 4",
+        "PlayStation 5",
+        "Xbox One",
+        "Xbox Series X|S",
+        "Nintendo Switch 2",
+        "macOS",
+    ],
+    multijugador: true,
+}
+
+//Convertir plataformas en un texto mediante join
+const plataformasListado = videojuego.plataformas.join(", ");
+//Convertir multijugador en "Sí" o "No" mediante un operador condicional.
+const juegoMultijugador = videojuego.multijugador ? "Si" : "No";
+//Reunir los datos en un template literal.
+
+const ficha = `
+FICHA DE VIDEOJUEGO
+===================
+Estudiante: ${nombre}
+Node.js: ${process.version}
+Plataforma del sistema: ${process.platform}
+Título: ${videojuego.titulo}
+Estudio: ${videojuego.estudio}
+Año: ${videojuego.anio}
+Plataformas: ${plataformasListado}
+¿Es multijugador?: ${juegoMultijugador}
+`;
+
+
+console.log(ficha);
