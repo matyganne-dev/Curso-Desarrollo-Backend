c76b537186f8bcc1c272687397a73170af594d06
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-08-21 03:02:44 -0300

    practica base con archivo hola.js

---
+++ semana-01/hola.js
@@ -0,0 +1,7 @@
+
+
+console.log("Hola desde Node.js");
+
+//Node.js proporciona el objeto global process . Para observar el entorno actual
+console.log("Versión de Node.js:", process.version);
+console.log("Plataforma:", process.platform);
