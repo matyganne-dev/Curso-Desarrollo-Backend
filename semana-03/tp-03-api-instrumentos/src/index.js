cb2dc02d18e9d1ba5021948c22a2b6cd19d5ef20
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-09-07 15:33:01 -0300

    Refactor estructura de la función main para mejorar la legibilidad y organización del código

--- semana-03/tp-03-api-instrumentos/src/index.js
+++ semana-03/tp-03-api-instrumentos/src/index.js
@@ -7,25 +7,50 @@ const PORT = 3000;
 const rutaDatos = path.join(__dirname, "..", "datos", "instrumentos.json");
 
 async function main() {
-  try {
-    const instrumentos = await leerJson(rutaDatos);
+    try {
+        const instrumentos = await leerJson(rutaDatos);
 
-    const app = express();
+        const app = express();
 
-    app.use(express.json());
+        app.use(express.json());
 
-    app.get("/", (req, res) => {
-      res.status(200).json({ mensaje: "API de Instrumentos Musicales disponible" });
-    });
+        app.get("/", (req, res) => {
+            //console.log("solicitud entrante", req);
+            res.status(200).json({ mensaje: "API de Instrumentos Musicales disponible" });
+        });
 
-    app.listen(PORT, () => {
-      console.log(`Servidor disponible en http://localhost:${PORT}`);
-    });
+        app.get("/api/instrumentos", (req, res) => {
+            const familia = req.query.familia;
 
-  } catch (error) {
-    console.error("No se pudo iniciar el servidor:", error.message);
-    process.exitCode = 1;
-  }
+            // /api/instrumentos?familia="cuerda"
+            // console.log("req.query.familia", familia);
+            // console.log("req.query", req.query);
+
+            //Si no existe parametro (eje: api/instrumentos?familia="cuerda"), traemos todo el listado
+            if (!familia) {
+                return res.status(200).json(instrumentos);
+            }
+
+            //Si existe parametro, filtramos
+            //console.log("instrumentos:", instrumentos);
+            const resultado = instrumentos.filter(
+                (instrumento) => instrumento.familia.toLowerCase() === String(familia).toLowerCase()
+            );
+            //console.log("\nresultado:", resultado);
+
+
+            //respuesta filtrada
+            res.status(200).json(resultado);
+        })
+
+        app.listen(PORT, () => {
+            console.log(`Servidor disponible en http://localhost:${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("No se pudo iniciar el servidor:", error.message);
+        process.exitCode = 1;
+    }
 }
 
 main();
