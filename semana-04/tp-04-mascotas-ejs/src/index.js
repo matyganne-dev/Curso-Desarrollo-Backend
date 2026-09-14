09339ff339cf74ef6bb9647967137439cc1e3d6a
Ganne Moreno Pablo Matias <matyganne@gmail.com>
2026-09-14 06:51:15 -0300

    Agregar configuración del servidor Express y manejo de archivos JSON

---
+++ semana-04/tp-04-mascotas-ejs/src/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("node:path");
+const expressLayouts = require("express-ejs-layouts");
+const leerJson = require("./archivos.js");
+
+
+const PORT = 3000;
+const rutaDatos = path.join(__dirname, "..", "datos", "mascotas.json");
+
+async function main() {
+    console.log("dirname",__dirname);
+    console.log("ruta:", rutaDatos);
+    try {
+        const mascotas = await leerJson(rutaDatos);
+
+        const app = express();
+
+        app.set("view engine", "ejs");
+        app.set("views", path.join(__dirname, "..", "views"));
+
+        app.use(expressLayouts);
+        app.set("layout", "layouts/main");
+
+        app.use(express.static(path.join(__dirname, "..", "public")));
+        app.use(express.urlencoded({ extended: false }));
+
+        app.get("/", (req, res) => {
+            res.send("Servidor configurado");
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Servidor de Mascotas disponible en http://localhost:${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("No se pudo iniciar el servidor:", error.message);
+        process.exitCode = 1;
+    }
+}
+
+main();
